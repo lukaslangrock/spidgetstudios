b0c1c653b15f8f3365e2a7a4ed901d7610aecda2
Lukas Langrock <lukas.langrock@mailbox.org>
2022-09-24 20:55:53 +0200

    setup pfeil

--- game/Assets/Scripts/Pfeil.cs
+++ game/Assets/Scripts/Pfeil.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Pfeil : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-      transform.LookAt(Vector3.zero);
+      transform.LookAt(target);
     }
 }
