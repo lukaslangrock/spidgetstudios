a4cb06df0459101dd39b6406b8924d337847ce3f
Leon Wilzer <leon.wilzer@protonmail.com>
2022-09-24 19:46:52 +0200

    Pfeilmodelle

---
+++ game/Assets/Scripts/Pfeil.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pfeil : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.LookAt(Vector3.zero);
+    }
+}
